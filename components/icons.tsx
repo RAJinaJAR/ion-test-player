53e5dfdac8b5cff40c3dde3946137396f3f30a32
Raj Dhapse <rajdhapse@gmail.com>
2025-09-29 02:10:49 +0530

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -23,3 +23,9 @@ export const ClockIcon: React.FC<{ className?: string }> = ({ className = "h-5 w
         <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
 );
+
+export const TrophyIcon: React.FC<{ className?: string }> = ({ className = "h-6 w-6" }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className}>
+        <path fillRule="evenodd" d="M16.5 3A3 3 0 0013.5 6H12v2.25L10.5 9.75 9 8.25V6H7.5A3 3 0 004.5 3c-1.096 0-2.04.57-2.592 1.411A.75.75 0 002.25 6v1.5c0 .35.18.674.471.854A5.996 5.996 0 017.5 9.75v3.31A7.5 7.5 0 0012 22.5a7.5 7.5 0 004.5-9.44V9.75a5.996 5.996 0 014.779-1.396.75.75 0 00.471-.854V6a.75.75 0 00-.342-1.589A3 3 0 0016.5 3zm-9 3H9v1.86l-1.5 1.5V6zm6 0h1.5v3.36l-1.5-1.5V6z" clipRule="evenodd" />
+    </svg>
+);
